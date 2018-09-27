aa220a82d03db5c60c48e3dbcead0e5060a3127f
cuongle <cuongle.dev@gmail.com>
2018-09-28 06:26:29 +0700

    cuongle update product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,7 +28,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $data['products'] = Product::with('user','category')->orderBy('id','desc')->paginate(10);
+        $data['products'] = Product::with('user', 'category')->orderBy('id', 'desc')->paginate(10);
         /*
          * $data['products'] = Product::where([
             ['name','like','%go%'],
@@ -36,14 +36,18 @@ class ProductController extends Controller
         ])->get();
         */
         //dd($data);
-        return view('admin.products.index',$data);
+        return view('admin.products.index', $data);
     }
-    public function create(){
-        $data['categories'] = Product::orderBy('name','asc')->get();
-        return view('admin.products.create',$data);
+
+    public function create()
+    {
+        $data['categories'] = Product::orderBy('name', 'asc')->get();
+        return view('admin.products.create', $data);
     }
-    public function store(Request $request){
-        $valid = Validator::make($request->all(),[
+
+    public function store(Request $request)
+    {
+        $valid = Validator::make($request->all(), [
             'name' => 'required',
             'code' => 'required|unique:products,code',
             'content' => 'required',
@@ -53,7 +57,7 @@ class ProductController extends Controller
             'quantity' => 'required|numeric|min:0',
             'category_id' => 'required|exists:categories,id',
             'image' => 'image|max:2048'
-        ],[
+        ], [
             'name.required' => 'Vui lòng nhập tên sản phẩm.',
             'code.required' => 'Vui lòng nhập mã sản phẩm.',
             'content.required' => 'Vui lòng nhập nội dung sản phẩm.',
@@ -79,25 +83,25 @@ class ProductController extends Controller
             'quantity.min' => 'Vui lòng nhập số nhỏ nhất là :min.'
         ]);
 
-        if ($valid->fails()){
+        if ($valid->fails()) {
             return redirect()->back()->withErrors($valid)->withInput();
-        }else{
+        } else {
             $imageName = '';
-            if($request->hasFile('image')){
+            if ($request->hasFile('image')) {
                 $image = $request->file('image');
-                if (file_exists(public_path('uploads'))){
+                if (file_exists(public_path('uploads'))) {
                     $folderName = date('Y-m');
-                    $fileNameWithTimestamp = md5($image->getClientOriginalName().time());
-                    $fileName =  $fileNameWithTimestamp."." . $image->getClientOriginalExtension();
-                    $thumbnailFileName = $fileNameWithTimestamp  ."_thumb." . $image->getClientOriginalExtension();
-                    if(!file_exists(public_path('uploads/'.$folderName))){
-                        mkdir(public_path('uploads/'.$folderName),0755);
+                    $fileNameWithTimestamp = md5($image->getClientOriginalName() . time());
+                    $fileName = $fileNameWithTimestamp . "." . $image->getClientOriginalExtension();
+                    $thumbnailFileName = $fileNameWithTimestamp . "_thumb." . $image->getClientOriginalExtension();
+                    if (!file_exists(public_path('uploads/' . $folderName))) {
+                        mkdir(public_path('uploads/' . $folderName), 0755);
                     }
                 }
 
                 // Di chuyen file vao uploads
-                $imageName = $folderName ."/". $fileName;
-                $image->move(public_path('uploads/'.$folderName),$fileName);
+                $imageName = $folderName . "/" . $fileName;
+                $image->move(public_path('uploads/' . $folderName), $fileName);
 
                 /*Image::make(public_path('uploads/'.$folderName."/".$fileName))
                     ->resize(200,150)
@@ -106,16 +110,15 @@ class ProductController extends Controller
             }
 
 
-
             $attributes = '';
-            if($request->has('attributes') && is_array($request->input('attributes')) && count($request->input('attributes')) > 0){
+            if ($request->has('attributes') && is_array($request->input('attributes')) && count($request->input('attributes')) > 0) {
                 $attributes = $request->input('attributes');
                 foreach ($attributes as $key => $item) {
-                    if(!isset($item['name'])){
+                    if (!isset($item['name'])) {
                         unset($attributes[$key]);
                         continue;
                     }
-                    if(!isset($item['value'])){
+                    if (!isset($item['value'])) {
                         unset($attributes[$key]);
                         continue;
                     }
@@ -139,13 +142,13 @@ class ProductController extends Controller
 
             // them tags
 
-            if($request->has('tags') && is_array($request->input('tags')) && count($request->input('tags'))>0){
+            if ($request->has('tags') && is_array($request->input('tags')) && count($request->input('tags')) > 0) {
                 $tags = $request->input('tags');
                 $tagsId = [];
                 foreach ($tags as $tag) {
                     $tag = Tag::firstOrCreate([
                         'name' => str_slug($tag)
-                    ],[
+                    ], [
                         'name' => str_slug($tag),
                         'slug' => str_slug($tag)
                     ]);
@@ -157,59 +160,160 @@ class ProductController extends Controller
             }
 
 
-
-            return redirect()->route('admin.product.index')->with('messager',"Thêm sản phẩm $product->name thành công.");
+            return redirect()->route('admin.product.index')->with('messager', "Thêm sản phẩm $product->name thành công.");
         }
 
     }
-    public function delete($id){
+
+    public function delete($id)
+    {
         $Product = Product::find($id);
-        if ($Product!==null){
+        if ($Product !== null) {
             $Product->delete();
-            return redirect()->route('admin.Product.index')->with('messager',"Xóa chuyên mục $Product->name thành công.");
+            return redirect()->route('admin.Product.index')->with('messager', "Xóa chuyên mục $Product->name thành công.");
         }
         return redirect()->route('admin.Product.index');
     }
+
     public function show($id)
     {
         $data['product'] = Product::find($id);
-        $data['categories'] = Product::orderBy('name','asc')->get();
-        if ($data['product']!==null){
-            return view('admin.products.show',$data);
+        $data['categories'] = Product::orderBy('name', 'asc')->get();
+        if ($data['product'] !== null) {
+            return view('admin.products.show', $data);
         }
-        return redirect()->route('admin.product.index')->with('error',"Không tìm thấy sản phẩm này.");
+        return redirect()->route('admin.product.index')->with('error', "Không tìm thấy sản phẩm này.");
 
     }
-    public function update(Request $request,$id){
-        $valid = Validator::make($request->all(),[
-            'name' => 'required|unique:products,name,'.$id,
-            'parent' => 'required',
-        ],[
-            'name.required' => 'Vui lòng nhập tên chuyên mục.',
-            'name.unique' => 'Chuyên mục đã bị trùng.Vui lòng nhập nhập chuyên mục khác.',
-            'parent.required' => 'Vui lòng chọn danh mục cha.',
-            'parent.exists' => 'Danh mục cha không hợp lệ.'
+
+    public function update(Request $request, $id)
+    {
+        $valid = Validator::make($request->all(), [
+            'name' => 'required',
+            'code' => 'required|unique:products,code,' . $id,
+            'content' => 'required',
+            'regular_price' => 'required|numeric|min:0',
+            'sale_price' => 'required|numeric|min:0',
+            'original_price' => 'required|numeric|min:0',
+            'quantity' => 'required|numeric|min:0',
+            'category_id' => 'required|exists:categories,id',
+            'image' => 'image|max:2048'
+        ], [
+            'name.required' => 'Vui lòng nhập tên sản phẩm.',
+            'code.required' => 'Vui lòng nhập mã sản phẩm.',
+            'content.required' => 'Vui lòng nhập nội dung sản phẩm.',
+            'category_id.required' => 'Vui lòng chọn danh mục.',
+            'category_id.exists' => 'Không tồn tại danh mục.',
+            'user_id.exists' => 'Không tồn tại danh mục.',
+            'image.image' => 'Không đúng chuẩn định dạng image.',
+            'image.size' => 'Dung lượng vượt quá giới hạn cho phép :max KB.',
+            // required
+            'regular_price.required' => 'Vui lòng nhập giá thị trường sản phẩm.',
+            'sale_price.required' => 'Vui lòng nhập giá bán sản phẩm.',
+            'original_price.required' => 'Vui lòng nhập giá gốc sản phẩm.',
+            'quantity.required' => 'Vui lòng nhập số lượng sản phẩm.',
+            // numeric
+            'regular_price.numeric' => 'Vui lòng nhập số.',
+            'sale_price.numeric' => 'Vui lòng nhập số.',
+            'original_price.numeric' => 'Vui lòng nhập số.',
+            'quantity.numeric' => 'Vui lòng nhập số.',
+            // min
+            'regular_price.min' => 'Vui lòng nhập số nhỏ nhất là :min.',
+            'sale_price.min' => 'Vui lòng nhập số nhỏ nhất là :min.',
+            'original_price.min' => 'Vui lòng nhập số nhỏ nhất là :min.',
+            'quantity.min' => 'Vui lòng nhập số nhỏ nhất là :min.'
         ]);
 
-        if ($valid->fails()){
+        if ($valid->fails()) {
             return redirect()->back()->withErrors($valid)->withInput();
-        }else{
-            $Product = Product::find($id);
-            if ($Product!==null){
-                $order = 0;
-                if($request->input('order')){
-                    $order = $request->input('order');
+        } else {
+            $product = Product::find($id);
+            if ($product !== null) {
+                $imageName = $product->image;
+                if ($request->hasFile('image')) {
+                    $image = $request->file('image');
+                    if (file_exists(public_path('uploads'))) {
+                        $folderName = date('Y-m');
+                        $fileNameWithTimestamp = md5($image->getClientOriginalName() . time());
+                        $fileName = $fileNameWithTimestamp . "." . $image->getClientOriginalExtension();
+                        $thumbnailFileName = $fileNameWithTimestamp . "_thumb." . $image->getClientOriginalExtension();
+                        if (!file_exists(public_path('uploads/' . $folderName))) {
+                            mkdir(public_path('uploads/' . $folderName), 0755);
+                        }
+                    }
+
+                    // Di chuyen file vao uploads
+                    $imageName = $folderName . "/" . $fileName;
+                    $image->move(public_path('uploads/' . $folderName), $fileName);
+
+                    /*Image::make(public_path('uploads/'.$folderName."/".$fileName))
+                        ->resize(200,150)
+                    ->save(public_path('uploads/'.$folderName."/".$thumbnailFileName));*/
+
+                    if( !is_dir(public_path('uploads/'.$product->image)) && file_exists(public_path('uploads/'.$product->image))){
+                        unlink(public_path('uploads/'.$product->image));
+                    }
+
                 }
-                $Product->name = $request->input('name');
-                $Product->email = $request->input('parent');
-                $Product->order = $order;
-                $Product->save();
-                return redirect()->route('admin.Product.index')->with('messager',"Cập nhật chuyên mục $Product->name thành công.");
+
+
+                $attributes = '';
+                if ($request->has('attributes') && is_array($request->input('attributes')) && count($request->input('attributes')) > 0) {
+                    $attributes = $request->input('attributes');
+                    foreach ($attributes as $key => $item) {
+                        if (!isset($item['name'])) {
+                            unset($attributes[$key]);
+                            continue;
+                        }
+                        if (!isset($item['value'])) {
+                            unset($attributes[$key]);
+                            continue;
+                        }
+                    }
+                    $attributes = json_encode($attributes);
+                }
+
+
+                $product->name = $request->input('name');
+                $product->code = mb_strtoupper($request->input('code'), 'UTF-8');
+                $product->content = $request->input('content');
+                $product->regular_price = $request->input('regular_price');
+                $product->sale_price = $request->input('sale_price');
+                $product->original_price = $request->input('original_price');
+                $product->quantity = $request->input('quantity');
+                $product->image = $imageName;
+                $product->attributes = $attributes;
+                $product->category_id = $request->input('category_id');
+                $product->user_id = auth()->id();
+
+                $product->save();
+
+                // them tags
+
+                if ($request->has('tags') && is_array($request->input('tags')) && count($request->input('tags')) > 0) {
+                    $tags = $request->input('tags');
+                    $tagsId = [];
+                    foreach ($tags as $tag) {
+                        $tag = Tag::firstOrCreate([
+                            'name' => str_slug($tag)
+                        ], [
+                            'name' => str_slug($tag),
+                            'slug' => str_slug($tag)
+                        ]);
+
+                        $tagsId[] = $tag->id;
+                    }
+
+                    $product->tags()->sync($tagsId);
+                }
+
+
+                return redirect()->route('admin.product.index')->with('messager', "Cập nhật sản phẩm $product->name thành công.");
 
             }
-            return redirect()->route('admin.Product.index')->with('error',"Không tìm thấy chuyên mục này.");
+            return redirect()->route('admin.product.index')->with('error', "Không tìm thấy sản phẩm này.");
 
-            }
+        }
 
     }
 }
